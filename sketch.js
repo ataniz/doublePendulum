d47e2cdb748960b885103b6cdb0ccbcc28c10c30
Ata Nizamoglu <nizamoglu@campus.tu-berlin.de>
2020-12-22 15:46:18 +0300

    doublependulum v1

--- sketch.js
+++ sketch.js
@@ -6,19 +6,40 @@ let a1 = 0;
 let a2 = 0;
 let a1_v = 0;
 let a2_v = 0;
-let a1_a = 0.001;
-let a2_a = -0.1;
+
 let px2 = -1;
 let py2 = -1;
+let g = 1;
 
 function setup() {
   createCanvas(1000, 1000);
+
+  a1 = PI / 2;
+  a2 = PI / 2;
+
   buffer = createGraphics(width, height);
   buffer.background(14, 39, 60);
   buffer.translate(500, 400);
 }
 
 function draw() {
+  // https://www.myphysicslab.com/pendulum/double-pendulum-en.html
+  let num1 =
+    -g * (2 * m1 + m2) * sin(a1) -
+    m2 * g * sin(a1 - 2 * a2) -
+    2 * sin(a1 - a2) * m2 * (a2_v ** 2 * l2 + a1_v ** 2 * l1 * cos(a1 - a2));
+  let den1 = l1 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2));
+  let a1_a = num1 / den1;
+
+  let num2 =
+    2 *
+    sin(a1 - a2) *
+    (a1_v ** 2 * l1 * (m1 + m2) +
+      g * (m1 + m2) * cos(a1) +
+      a2_v ** 2 * l2 * m2 * cos(a1 - a2));
+  let den2 = l2 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2));
+  let a2_a = num2 / den2;
+
   background(14, 39, 60);
   image(buffer, 0, 0, width, height);
 
@@ -39,10 +60,10 @@ function draw() {
   ellipse(x1, y1, m1, m1);
   ellipse(x2, y2, m1, m1);
 
-  a1 += a1_v;
-  a2 += a2_v;
   a1_v += a1_a;
   a2_v += a2_a;
+  a1 += a1_v;
+  a2 += a2_v;
 
   buffer.stroke(232, 93, 117);
   if (frameCount > 1) {
