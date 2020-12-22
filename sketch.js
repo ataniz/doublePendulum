cc79f4fe4b3f2d0128e0322facd5b6d2be581a12
Ata Nizamoglu <nizamoglu@campus.tu-berlin.de>
2020-12-22 15:10:08 +0300

    drawing a flower

--- sketch.js
+++ sketch.js
@@ -3,18 +3,29 @@ let l2 = 100;
 let m1 = 10;
 let m2 = 10;
 let a1 = 0;
-let a1 = 0;
+let a2 = 0;
+let a1_v = 0;
+let a2_v = 0;
+let a1_s = 0;
+let a2_s = 0;
+let px2 = -1;
+let py2 = -1;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1000, 1000);
+  buffer = createGraphics(width, height);
+  buffer.background(14, 39, 60);
+  buffer.translate(500, 400);
 }
 
 function draw() {
-  background(225);
-  stroke(0);
+  background(14, 39, 60);
+  image(buffer, 0, 0, width, height);
+
+  stroke(204, 204, 204);
   strokeWeight(2);
 
-  translate(300, 50);
+  translate(500, 400);
 
   let x1 = l1 * sin(a1);
   let x2 = x1 + l2 * sin(a2);
@@ -22,6 +33,20 @@ function draw() {
   let y2 = y1 + l2 * cos(a2);
 
   line(0, 0, x1, y1);
-  fill(0);
+  fill(204, 204, 204);
+  line(x1, y1, x2, y2);
+  fill(204, 204, 204);
   ellipse(x1, y1, m1, m1);
+  ellipse(x2, y2, m1, m1);
+
+  a1 += 0.02;
+  a2 -= 0.05;
+
+  buffer.stroke(232, 93, 117);
+  if (frameCount > 1) {
+    buffer.line(px2, py2, x2, y2);
+  }
+
+  px2 = x2;
+  py2 = y2;
 }
